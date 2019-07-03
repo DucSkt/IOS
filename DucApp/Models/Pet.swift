657ad26655fe3ec94c9467aa6e922f6bf0563b18
DUC SKT <van-duc.hoang@ant-tech.eu>
2019-07-03 23:43:52 +0700

    project

---
+++ DucApp/Models/Pet.swift
@@ -0,0 +1,17 @@
+//
+//  Pet.swift
+//  DucApp
+//
+//  Created by DUC SKT on 7/3/19.
+//  Copyright © 2019 DUC SKT. All rights reserved.
+//
+
+import Foundation
+struct Pet {
+    var name: String?
+    var title: String?
+    init(name: String?, title: String?) {
+        self.name = name
+        self.title = title 
+    }
+}
---
+++ DucApp/Models/SimpleData.swift
@@ -0,0 +1,13 @@
+//
+//  SimpleData.swift
+//  DucApp
+//
+//  Created by DUC SKT on 7/3/19.
+//  Copyright © 2019 DUC SKT. All rights reserved.
+//
+
+import Foundation
+let petsData = [
+    Pet(  name: "cc", title: "dd"),
+    Pet(  name: "aa", title: "bb"),
+]
